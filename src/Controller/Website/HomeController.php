6a8e2e2b2dd8acc7b5b8dcdc4034dec326bbb659
Kamil Wilas <kamil@kramsoft.pl>
2020-06-24 15:38:19 +0200

    Move renderStructure method to contoller
    This will make possible to add form as attribute
    
    https://trello.com/c/B0rOjl8M

--- src/Controller/Website/HomeController.php
+++ src/Controller/Website/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Website;
 use Sulu\Bundle\WebsiteBundle\Controller\WebsiteController;
 use Sulu\Component\Content\Compat\StructureInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class HomeController extends WebsiteController
 {
@@ -16,6 +17,60 @@ class HomeController extends WebsiteController
             $preview,
             $partial
         );
+        return $response;
+    }
+
+    protected function renderStructure(
+        StructureInterface $structure,
+        $attributes = [],
+        $preview = false,
+        $partial = false
+    ) {
+        // extract format twig file
+        if (!$preview) {
+            $request = $this->getRequest();
+            $requestFormat = $request->getRequestFormat();
+        } else {
+            $requestFormat = 'html';
+        }
+
+        $viewTemplate = $structure->getView() . '.' . $requestFormat . '.twig';
+
+        if (!$this->get('twig')->getLoader()->exists($viewTemplate)) {
+            throw new HttpException(
+                406,
+                sprintf('Page does not exist in "%s" format.', $requestFormat)
+            );
+        }
+
+        // get attributes to render template
+        $data = $this->getAttributes($attributes, $structure, $preview);
+
+        // if partial render only content block else full page
+        if ($partial) {
+            $content = $this->renderBlock(
+                $viewTemplate,
+                'content',
+                $data
+            );
+        } elseif ($preview) {
+            $content = $this->renderPreview(
+                $viewTemplate,
+                $data
+            );
+        } else {
+            $content = $this->renderView(
+                $viewTemplate,
+                $data
+            );
+        }
+
+        $response = new Response($content);
+
+        if (!$preview && $this->getCacheTimeLifeEnhancer()) {
+            $this->getCacheTimeLifeEnhancer()->enhance($response, $structure);
+        }
+
         return $response;
     }
 }
