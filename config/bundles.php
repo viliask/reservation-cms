dbe9e5ae0292a50732ad08e8fb6342b5841616d6
Kamil Wilas <kamil.wilas@outlook.com>
2020-06-27 13:06:34 +0200

    Add config for new packages
    https://trello.com/c/A0Y4daKQ

--- config/bundles.php
+++ config/bundles.php
@@ -45,4 +45,6 @@ return [
     Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle::class => ['all' => true, 'website' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
