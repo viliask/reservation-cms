4928250bd3ff3ece3da48615e9f32003d9444e7a
Kamil Wilas <kamil.wilas@outlook.com>
2020-06-29 12:58:05 +0200

    Add event entity
    https://trello.com/c/A0Y4daKQ

---
+++ src/Entity/Event.php
@@ -0,0 +1,240 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EventRepository")
+ */
+class Event
+{
+    const RESOURCE_KEY = 'events';
+
+    /**
+     * @var int|null
+     *
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(type="boolean", nullable=false)
+     */
+    private $enabled;
+
+    /**
+     * @var \DateTimeImmutable|null
+     *
+     * @ORM\Column(type="datetime_immutable", nullable=true)
+     */
+    private $startDate;
+
+    /**
+     * @var \DateTimeImmutable|null
+     *
+     * @ORM\Column(type="datetime_immutable", nullable=true)
+     */
+    private $endDate;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $location;
+
+    /**
+     * @var EventTranslation[]|Collection
+     *
+     * @ORM\OneToMany(targetEntity="App\Entity\EventTranslation", mappedBy="event", cascade={"ALL"}, indexBy="locale")
+     *
+     * @Serializer\Exclude
+     */
+    private $translations;
+
+    /**
+     * @var string
+     */
+    private $locale;
+
+    public function __construct()
+    {
+        $this->enabled = false;
+        $this->translations = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function isEnabled(): bool
+    {
+        return $this->enabled;
+    }
+
+    public function setEnabled(bool $enabled): self
+    {
+        $this->enabled = $enabled;
+
+        return $this;
+    }
+
+    public function getStartDate(): ?\DateTimeImmutable
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate(?\DateTimeImmutable $startDate): self
+    {
+        $this->startDate = $startDate;
+
+        return $this;
+    }
+
+    public function getEndDate(): ?\DateTimeImmutable
+    {
+        return $this->endDate;
+    }
+
+    public function setEndDate(?\DateTimeImmutable $endDate): self
+    {
+        $this->endDate = $endDate;
+
+        return $this;
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(?string $location): self
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    /**
+     * @Serializer\VirtualProperty(name="title")
+     */
+    public function getTitle(): ?string
+    {
+        $translation = $this->getTranslation($this->locale);
+        if (!$translation) {
+            return null;
+        }
+
+        return $translation->getTitle();
+    }
+
+    public function setTitle(string $title): self
+    {
+        $translation = $this->getTranslation($this->locale);
+        if (!$translation) {
+            $translation = $this->createTranslation($this->locale);
+        }
+
+        $translation->setTitle($title);
+
+        return $this;
+    }
+
+    /**
+     * @Serializer\VirtualProperty(name="teaser")
+     */
+    public function getTeaser(): ?string
+    {
+        $translation = $this->getTranslation($this->locale);
+        if (!$translation) {
+            return null;
+        }
+
+        return $translation->getTeaser();
+    }
+
+    public function setTeaser(string $teaser): self
+    {
+        $translation = $this->getTranslation($this->locale);
+        if (!$translation) {
+            $translation = $this->createTranslation($this->locale);
+        }
+
+        $translation->setTeaser($teaser);
+
+        return $this;
+    }
+
+    /**
+     * @Serializer\VirtualProperty(name="description")
+     */
+    public function getDescription(): ?string
+    {
+        $translation = $this->getTranslation($this->locale);
+        if (!$translation) {
+            return null;
+        }
+
+        return $translation->getDescription();
+    }
+
+    public function setDescription(string $description): self
+    {
+        $translation = $this->getTranslation($this->locale);
+        if (!$translation) {
+            $translation = $this->createTranslation($this->locale);
+        }
+
+        $translation->setDescription($description);
+
+        return $this;
+    }
+
+    public function getLocale(): string
+    {
+        return $this->locale;
+    }
+
+    public function setLocale(string $locale): self
+    {
+        $this->locale = $locale;
+
+        return $this;
+    }
+
+    /**
+     * @return EventTranslation[]
+     */
+    public function getTranslations(): array
+    {
+        return $this->translations->toArray();
+    }
+
+    protected function getTranslation(string $locale): ?EventTranslation
+    {
+        if (!$this->translations->containsKey($locale)) {
+            return null;
+        }
+
+        return $this->translations->get($locale);
+    }
+
+    protected function createTranslation(string $locale): EventTranslation
+    {
+        $translation = new EventTranslation($this, $locale);
+        $this->translations->set($locale, $translation);
+
+        return $translation;
+    }
+}
---
+++ src/Repository/EventRepository.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\Event;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\Query\Expr\Join;
+use Doctrine\ORM\QueryBuilder;
+use Sulu\Component\SmartContent\Orm\DataProviderRepositoryInterface;
+use Sulu\Component\SmartContent\Orm\DataProviderRepositoryTrait;
+
+/**
+ * @method Event|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Event|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Event[]    findAll()
+ * @method Event[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class EventRepository extends ServiceEntityRepository implements DataProviderRepositoryInterface
+{
+    use DataProviderRepositoryTrait {
+        findByFilters as parentFindByFilters;
+    }
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Event::class);
+    }
+
+    public function create(string $locale): Event
+    {
+        $event = new Event();
+        $event->setLocale($locale);
+
+        return $event;
+    }
+
+    public function remove(int $id): void
+    {
+        $this->getEntityManager()->remove(
+            $this->getEntityManager()->getReference(
+                $this->getClassName(),
+                $id
+            )
+        );
+        $this->getEntityManager()->flush();
+    }
+
+    public function save(Event $event): void
+    {
+        $this->getEntityManager()->persist($event);
+        $this->getEntityManager()->flush();
+    }
+
+    public function findById(int $id, string $locale): ?Event
+    {
+        $event = $this->find($id);
+        if (!$event) {
+            return null;
+        }
+
+        $event->setLocale($locale);
+
+        return $event;
+    }
+
+    public function findByFilters($filters, $page, $pageSize, $limit, $locale, $options = [])
+    {
+        $entities = $this->parentFindByFilters($filters, $page, $pageSize, $limit, $locale, $options);
+
+        return array_map(
+            function (Event $entity) use ($locale) {
+                return $entity->setLocale($locale);
+            },
+            $entities
+        );
+    }
+
+    protected function appendJoins(QueryBuilder $queryBuilder, string $alias, string $locale): void
+    {
+        $queryBuilder->innerJoin($alias . '.translations', 'translation', Join::WITH, 'translation.locale = :locale');
+        $queryBuilder->setParameter('locale', $locale);
+
+        $queryBuilder->andWhere($alias . '.enabled = true');
+    }
+}
