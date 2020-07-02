ec5481d337600541bd42e7bd655e0ac44d8061cc
Kamil Wilas <kamil.wilas@outlook.com>
2020-07-02 16:24:16 +0200

    Update room repository to work with sulu admin form
    https://trello.com/c/A0Y4daKQ

--- src/Repository/RoomRepository.php
+++ src/Repository/RoomRepository.php
@@ -1,10 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Repository;
 
 use App\Entity\Room;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\Query\Expr\Join;
+use Doctrine\ORM\QueryBuilder;
+use Sulu\Component\SmartContent\Orm\DataProviderRepositoryInterface;
+use Sulu\Component\SmartContent\Orm\DataProviderRepositoryTrait;
 
 /**
  * @method Room|null find($id, $lockMode = null, $lockVersion = null)
@@ -12,39 +18,71 @@ use Doctrine\Common\Persistence\ManagerRegistry;
  * @method Room[]    findAll()
  * @method Room[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class RoomRepository extends ServiceEntityRepository
+class RoomRepository extends ServiceEntityRepository implements DataProviderRepositoryInterface
 {
+    use DataProviderRepositoryTrait {
+        findByFilters as parentFindByFilters;
+    }
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Room::class);
     }
 
-    // /**
-    //  * @return Room[] Returns an array of Room objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    public function create(string $locale): Room
     {
-        return $this->createQueryBuilder('r')
-            ->andWhere('r.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('r.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
+        $room = new Room();
+        $room->setLocale($locale);
 
-    /*
-    public function findOneBySomeField($value): ?Room
-    {
-        return $this->createQueryBuilder('r')
-            ->andWhere('r.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
+        return $room;
     }
-    */
+
+    public function remove(int $id): void
+    {
+        $this->getEntityManager()->remove(
+            $this->getEntityManager()->getReference(
+                $this->getClassName(),
+                $id
+            )
+        );
+        $this->getEntityManager()->flush();
+    }
+
+    public function save(Room $room): void
+    {
+        $this->getEntityManager()->persist($room);
+        $this->getEntityManager()->flush();
+    }
+
+    public function findById(int $id, string $locale): ?Room
+    {
+        $room = $this->find($id);
+        if (!$room) {
+            return null;
+        }
+
+        $room->setLocale($locale);
+
+        return $room;
+    }
+
+    public function findByFilters($filters, $page, $pageSize, $limit, $locale, $options = [])
+    {
+        $entities = $this->parentFindByFilters($filters, $page, $pageSize, $limit, $locale, $options);
+
+        return array_map(
+            function (Room $entity) use ($locale) {
+                return $entity->setLocale($locale);
+            },
+            $entities
+        );
+    }
+
+    protected function appendJoins(QueryBuilder $queryBuilder, string $alias, string $locale): void
+    {
+        $queryBuilder->innerJoin($alias . '.translations', 'translation', Join::WITH, 'translation.locale = :locale');
+        $queryBuilder->setParameter('locale', $locale);
+
+        $queryBuilder->andWhere($alias . '.enabled = true');
+    }
 }
