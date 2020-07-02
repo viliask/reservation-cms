9af294911a9494db7f8d47ba26b74e7cd32754c7
Kamil Wilas <kamil.wilas@outlook.com>
2020-07-02 16:44:39 +0200

    Integarate room entity with its translation
    https://trello.com/c/A0Y4daKQ

--- src/Entity/Room.php
+++ src/Entity/Room.php
@@ -6,6 +6,7 @@ use App\Repository\RoomRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
 
 /**
  * @ORM\Entity(repositoryClass=RoomRepository::class)
@@ -22,6 +23,13 @@ class Room
     private $id;
 
     /**
+     * @var bool
+     *
+     * @ORM\Column(type="boolean", nullable=false)
+     */
+    private $enabled;
+
+    /**
      * @ORM\Column(type="string", length=50)
      */
     private $name;
@@ -36,14 +44,42 @@ class Room
      */
     private $reservations;
 
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $locale;
+
+    /**
+     * @var RoomTranslation[]|Collection
+     *
+     * @ORM\OneToMany(targetEntity="App\Entity\RoomTranslation", mappedBy="room", cascade={"ALL"}, indexBy="locale")
+     *
+     * @Serializer\Exclude
+     */
+    private $translations;
+
     public function __construct()
     {
+        $this->enabled = false;
+        $this->translations = new ArrayCollection();
         $this->reservations = new ArrayCollection();
     }
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function isEnabled(): bool
+    {
+        return $this->enabled;
+    }
+
+    public function setEnabled(bool $enabled): self
+    {
+        $this->enabled = $enabled;
+
+        return $this;
     }
 
     public function getName(): ?string
@@ -96,6 +132,118 @@ class Room
         }
 
         return $this;
+    }
+
+    /**
+     * @Serializer\VirtualProperty(name="title")
+     */
+    public function getTitle(): ?string
+    {
+        $translation = $this->getTranslation($this->locale);
+        if (!$translation) {
+            return null;
+        }
+
+        return $translation->getTitle();
+    }
+
+    public function setTitle(string $title): self
+    {
+        $translation = $this->getTranslation($this->locale);
+        if (!$translation) {
+            $translation = $this->createTranslation($this->locale);
+        }
+
+        $translation->setTitle($title);
+
+        return $this;
+    }
+
+    /**
+     * @Serializer\VirtualProperty(name="teaser")
+     */
+    public function getTeaser(): ?string
+    {
+        $translation = $this->getTranslation($this->locale);
+        if (!$translation) {
+            return null;
+        }
+
+        return $translation->getTeaser();
+    }
+
+    public function setTeaser(string $teaser): self
+    {
+        $translation = $this->getTranslation($this->locale);
+        if (!$translation) {
+            $translation = $this->createTranslation($this->locale);
+        }
+
+        $translation->setTeaser($teaser);
+
+        return $this;
+    }
+
+    /**
+     * @Serializer\VirtualProperty(name="description")
+     */
+    public function getDescription(): ?string
+    {
+        $translation = $this->getTranslation($this->locale);
+        if (!$translation) {
+            return null;
+        }
+
+        return $translation->getDescription();
+    }
+
+    public function setDescription(string $description): self
+    {
+        $translation = $this->getTranslation($this->locale);
+        if (!$translation) {
+            $translation = $this->createTranslation($this->locale);
+        }
+
+        $translation->setDescription($description);
+
+        return $this;
+    }
+
+    public function getLocale(): string
+    {
+        return $this->locale;
+    }
+
+    public function setLocale(string $locale): self
+    {
+        $this->locale = $locale;
+
+        return $this;
+    }
+
+    /**
+     * @return RoomTranslation[]
+     */
+    public function getTranslations(): array
+    {
+        return $this->translations->toArray();
+    }
+
+    protected function getTranslation(string $locale): ?RoomTranslation
+    {
+        if (!$this->translations->containsKey($locale)) {
+            return null;
+        }
+
+        return $this->translations->get($locale);
+    }
+
+    protected function createTranslation(string $locale): RoomTranslation
+    {
+        $translation = new RoomTranslation($this, $locale);
+        $this->translations->set($locale, $translation);
+
+        return $translation;
     }
 
     public function __toString()
