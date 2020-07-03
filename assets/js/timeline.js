101aa438f0e726e4172f45ef04d9d9a1b7215be2
Kamil Wilas <kamil.wilas@outlook.com>
2020-07-03 09:25:54 +0200

    Add dummy groups
    https://trello.com/c/4XEg94s5

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -6,13 +6,24 @@ const container = document.getElementById('visualization');
 
 // Create a DataSet (allows two way data-binding)
 const items = new DataSet([
-    { id: 1, content: 'item 1', start: '2014-04-20' },
-    { id: 2, content: 'item 2', start: '2014-04-14' },
-    { id: 3, content: 'item 3', start: '2014-04-18' },
-    { id: 4, content: 'item 4', start: '2014-04-16', end: '2014-04-19' },
-    { id: 5, content: 'item 5', start: '2014-04-25' },
-    { id: 6, content: 'item 6', start: '2014-04-27', type: 'point' }
+    { id: 1, group: 1, content: 'item 1', start: '2014-04-20' },
+    { id: 2, group: 1, content: 'item 2', start: '2014-04-14' },
+    { id: 3, group: 1, content: 'item 3', start: '2014-04-18' },
+    { id: 4, group: 2, content: 'item 4', start: '2014-04-16', end: '2014-04-19' },
+    { id: 5, group: 2, content: 'item 5', start: '2014-04-25' },
+    { id: 6, group: 2, content: 'item 6', start: '2014-04-27', type: 'point' }
 ]);
+
+const groups = [
+    {
+        id: 1,
+        content: 'Group 1'
+    },
+    {
+        id: 2,
+        content: 'Group 2'
+    }
+];
 
 // Configuration for the Timeline
 const options = {
@@ -21,4 +32,4 @@ const options = {
 };
 
 // Create a Timeline
-const timeline = new Timeline(container, items, options);
+const timeline = new Timeline(container, items, groups, options);
