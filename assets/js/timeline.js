236106a498c55ace26793d4c0c3f9664b5c79079
Kamil Wilas <kamil.wilas@outlook.com>
2020-07-04 23:41:49 +0200

    Delete redundant objects
    https://trello.com/c/4XEg94s5

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -9,27 +9,6 @@ Routing.setRoutingData(routes);
 // DOM element where the Timeline will be attached
 const container = document.getElementById('visualization');
 
-// Create a DataSet (allows two way data-binding)
-const items = new DataSet([
-    { id: 1, group: 1, content: 'item 1', start: '2014-04-20' },
-    { id: 2, group: 1, content: 'item 2', start: '2014-04-14' },
-    { id: 3, group: 1, content: 'item 3', start: '2014-04-18' },
-    { id: 4, group: 2, content: 'item 4', start: '2014-04-16', end: '2014-04-19' },
-    { id: 5, group: 2, content: 'item 5', start: '2014-04-25' },
-    { id: 6, group: 2, content: 'item 6', start: '2014-04-27', type: 'point' }
-]);
-
-const groups = [
-    {
-        id: 1,
-        content: 'Group 1'
-    },
-    {
-        id: 2,
-        content: 'Group 2'
-    }
-];
-
 // Configuration for the Timeline
 const options = {
     start: '2014-04-01',
@@ -37,7 +16,7 @@ const options = {
 };
 
 // Create a Timeline
-const timeline = new Timeline(container, items, groups, options);
+const timeline = new Timeline(container, options);
 
 document.addEventListener('DOMContentLoaded',  () => {
     loadData();
