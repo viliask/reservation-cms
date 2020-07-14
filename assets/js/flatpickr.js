3e28ea058981c715a994eaaf6e4949abca1c469e
Kamil Wilas <kamil.wilas@outlook.com>
2020-07-14 18:38:03 +0200

    Define config
    https://trello.com/c/VALZlg7Q

--- assets/js/flatpickr.js
+++ assets/js/flatpickr.js
@@ -1,6 +1,9 @@
 import flatpickr from 'flatpickr';
 import rangePlugin from 'flatpickr/dist/plugins/rangePlugin';
+import pl from 'flatpickr/dist/l10n/pl';
 
 flatpickr('.js-datepicker', {
+    minDate: 'today',
+    locale: 'pl',
     plugins: [new rangePlugin({ input: "#reservation_checkOutDate"})]
 });
