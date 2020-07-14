0162610e01128a26141657ad059a89c941b173a6
Kamil Wilas <kamil.wilas@outlook.com>
2020-07-14 11:38:04 +0200

    Rewrite timeline implementation from js to React
    https://trello.com/c/4XEg94s5

--- assets/admin/AdminBundle/Resources/js/containers/Timeline.js
+++ assets/admin/AdminBundle/Resources/js/containers/Timeline.js
@@ -1,6 +1,46 @@
 import React from 'react';
+import 'vis-timeline/styles/vis-timeline-graph2d.css';
+import Timeline from 'react-vis-timeline'
+import Axios from 'axios';
+import Routing from '../../../../../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
+
+const routes = require('../../../../../../public/js/fos_js_routes.json');
+Routing.setRoutingData(routes);
+
+let items, groups;
+
+const date = new Date(), y = date.getFullYear(), m = date.getMonth();
+const firstDOM = new Date(y, m, 1);
+const lastDOM = new Date(y, m + 1, 0);
+
+const options = {
+    start: firstDOM,
+    end: lastDOM
+};
 
 class Timeline extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: null,
+            groups: null,
+        };
+    }
+
+    componentDidMount() {
+        this.loadData();
+    }
+
+    loadData() {
+        Axios.get(Routing.generate('app.imeline_items', null, true)).then((response) => {
+            items = response.data;
+        }).then(() => {
+            Axios.get(Routing.generate('app.imeline_groups', null, true)).then((response) => {
+                groups = response.data;
+            });
+        });
+    }
 
     render() {
         return <div id="visualization">Test</div>;
