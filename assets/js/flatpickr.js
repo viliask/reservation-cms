87fb88c40393ef4b7cb8a9d954d96955ea39c261
Kamil Wilas <kamil.wilas@outlook.com>
2020-07-14 18:25:11 +0200

    Add range plugin
    https://trello.com/c/VALZlg7Q

--- assets/js/flatpickr.js
+++ assets/js/flatpickr.js
@@ -1,4 +1,6 @@
 import flatpickr from 'flatpickr';
+import rangePlugin from 'flatpickr/dist/plugins/rangePlugin';
 
 flatpickr('.js-datepicker', {
+    plugins: [new rangePlugin({ input: "#reservation_checkOutDate"})]
 });
