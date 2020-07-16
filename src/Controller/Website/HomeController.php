c6a81ea389eb47d0bd30fa6d873d22abe43ad615
Kamil Wilas <kamil.wilas@outlook.com>
2020-07-16 14:53:22 +0200

    Move form creation to action
    https://trello.com/c/ISgjpISC

--- src/Controller/Website/HomeController.php
+++ src/Controller/Website/HomeController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class HomeController extends WebsiteController
 {
-    const RESERVATION_PATH = '/reservation';
+    const RESERVATION_PATH = 'reservation';
 
     /**
      * @param \Sulu\Component\Content\Compat\StructureInterface $structure
@@ -21,80 +21,16 @@ class HomeController extends WebsiteController
      */
     public function indexAction(StructureInterface $structure, bool $preview = false, bool $partial = false)
     {
+        $form = $this->createForm(ReservationType::class, null, ['action' => $this->generateUrl(self::RESERVATION_PATH)]);
+        $attributes = [];
+        $attributes['form'] = $form->createView();
+
         $response = $this->renderStructure(
             $structure,
-            [],
+            $attributes,
             $preview,
             $partial
         );
-        return $response;
-    }
-
-    /**
-     * @param StructureInterface $structure The structure, which has been loaded for rendering
-     * @param array $attributes Additional attributes, which will be passed to twig
-     * @param bool $preview Defines if the site is rendered in preview mode
-     * @param bool $partial Defines if only the content block of the template should be rendered
-     *
-     * @return Response
-     */
-    protected function renderStructure(
-        StructureInterface $structure,
-        $attributes = [],
-        $preview = false,
-        $partial = false
-    ) {
-        // extract format twig file
-        if (!$preview) {
-            $request = $this->getRequest();
-            $requestFormat = $request->getRequestFormat();
-        } else {
-            $requestFormat = 'html';
-        }
-
-        $viewTemplate = $structure->getView() . '.' . $requestFormat . '.twig';
-
-        if (!$this->get('twig')->getLoader()->exists($viewTemplate)) {
-            throw new HttpException(
-                406,
-                sprintf('Page does not exist in "%s" format.', $requestFormat)
-            );
-        }
-
-//      TODO: Make generateUri usable
-//        $form = $this->createForm(ReservationType::class, null, ['action' => $this->generateUrl('reservation')]);
-        $form = $this->createForm(ReservationType::class, null, ['action' => self::RESERVATION_PATH]);
-
-        $attributes = ['form' => $form->createView()];
-
-        // get attributes to render template
-        $data = $this->getAttributes($attributes, $structure, $preview);
-
-        // if partial render only content block else full page
-        if ($partial) {
-            $content = $this->renderBlock(
-                $viewTemplate,
-                'content',
-                $data
-            );
-        } elseif ($preview) {
-            $content = $this->renderPreview(
-                $viewTemplate,
-                $data
-            );
-        } else {
-            $content = $this->renderView(
-                $viewTemplate,
-                $data
-            );
-        }
-
-        $response = new Response($content);
-
-        if (!$preview && $this->getCacheTimeLifeEnhancer()) {
-            $this->getCacheTimeLifeEnhancer()->enhance($response, $structure);
-        }
-
         return $response;
     }
 
