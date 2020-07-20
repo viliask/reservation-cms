af55eef22dded3f3808d0e1752b432315b1415ef
Kamil Wilas <kamil.wilas@outlook.com>
2020-07-20 17:42:56 +0200

    Fix listeners
    https://trello.com/c/h1DEDMB6

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,7 +1,6 @@
 const openEl = document.querySelector('[data-open]');
 const closeEl = document.querySelector('[data-close]');
 const visible = 'visible';
-const openedModal = document.querySelector('.modal.visible');
 const makeInvisible = (modal) => {
     modal.classList.remove(visible);
 };
@@ -16,12 +15,16 @@ closeEl.addEventListener('click', function () {
 });
 
 document.addEventListener('click', e => {
+    const openedModal = document.querySelector('.modal.visible');
+
     if (e.target === openedModal) {
         makeInvisible(openedModal);
     }
 });
 
 document.addEventListener('keyup', e => {
+    const openedModal = document.querySelector('.modal.visible');
+
     if (e.key === 'Escape' && openedModal) {
         makeInvisible(openedModal);
     }
