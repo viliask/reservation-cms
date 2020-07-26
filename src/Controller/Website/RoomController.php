3b8257130d5d4ad53fb0f8092eb0e22e40f1bdb4
Kamil Wilas <kamil.wilas@outlook.com>
2020-07-26 17:29:50 +0200

    Include event form
    https://trello.com/c/h1DEDMB6

--- src/Controller/Website/RoomController.php
+++ src/Controller/Website/RoomController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller\Website;
 
+use App\Entity\Event;
 use App\Entity\Room;
+use App\Form\Type\EventType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,10 +18,21 @@ class RoomController extends AbstractController
     /**
      * @Route("/{id}", name="room_show", methods={"GET"})
      */
-    public function show(Room $room): Response
+    public function show(Room $room, Request $request): Response
     {
+        $event = new Event();
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($event);
+            $entityManager->flush();
+        }
+
         return $this->render('room/show.html.twig', [
             'room' => $room,
+            'form' => $form->createView(),
         ]);
     }
 }
