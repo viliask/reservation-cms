917067273f39d54dec18a139d48a28e01fe87236
Kamil Wilas <kamil.wilas@outlook.com>
2020-08-05 13:02:30 +0200

    Pass dates to twig
    https://trello.com/c/h1DEDMB6

--- src/Controller/Website/HomeController.php
+++ src/Controller/Website/HomeController.php
@@ -38,8 +38,10 @@ class HomeController extends WebsiteController
     public function reservation(Request $request, EventRepository $eventRepository): Response
     {
         $reservation = $request->request->get('reservation');
-        $rooms       = $eventRepository->findAvailableRooms($reservation['checkInDate'], $reservation['checkOutDate']);
+        $checkIn     = $reservation['checkInDate'];
+        $checkOut    = $reservation['checkOutDate'];
+        $rooms       = $eventRepository->findAvailableRooms($checkIn, $checkOut);
 
-        return $this->render('/room/index.html.twig', ['rooms' => $rooms]);
+        return $this->render('/room/index.html.twig', ['rooms' => $rooms, 'checkIn' => $checkIn, 'checkOut' => $checkOut]);
     }
 }
