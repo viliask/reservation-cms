a9a1d8d4e507a82d6d3e815f589755701859b5c7
Kamil Wilas <kamil.wilas@outlook.com>
2020-08-12 11:24:57 +0200

    Add assets
    https://trello.com/c/VPYsojr3

---
+++ assets/js/modal.js
@@ -0,0 +1,45 @@
+const openEl = document.querySelector('[data-open]');
+const closeEl = document.querySelector('[data-close]');
+const visible = 'visible';
+const PRICE = 40;
+
+const makeInvisible = (modal) => {
+    modal.classList.remove(visible);
+};
+
+const updatePrice = () => {
+    const checkIn = new Date(document.querySelector('#event_checkIn').value);
+    const checkOut = new Date(document.querySelector('#event_checkOut').value);
+    const guests = document.querySelector('#event_guests').value;
+    const daysOfVisit = (checkOut.getTime() - checkIn.getTime()) / (1000 * 3600 * 24);
+    document.querySelector('#event_price').value = (daysOfVisit * PRICE * guests);
+};
+
+openEl.addEventListener('click', () => {
+    document.getElementById('reservation-modal').classList.add(visible);
+    updatePrice();
+});
+
+closeEl.addEventListener('click', () => {
+    document.getElementById('reservation-modal').classList.remove(visible);
+});
+
+document.addEventListener('click', e => {
+    const openedModal = document.querySelector('.modal.visible');
+
+    if (e.target === openedModal) {
+        makeInvisible(openedModal);
+    }
+});
+
+document.addEventListener('keyup', e => {
+    const openedModal = document.querySelector('.modal.visible');
+
+    if (e.key === 'Escape' && openedModal) {
+        makeInvisible(openedModal);
+    }
+});
+
+document.querySelector('#event_guests').addEventListener('change', () => {
+    updatePrice();
+});
