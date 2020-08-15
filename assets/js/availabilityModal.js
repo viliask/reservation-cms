1a34f1fee318cbd1028ae9c368dd3aa4d6384f98
Kamil Wilas <kamil.wilas@outlook.com>
2020-08-15 16:37:14 +0200

    Add loadData function to get data  for modal
    https://trello.com/c/FhumAsQ7

--- assets/js/availabilityModal.js
+++ assets/js/availabilityModal.js
@@ -3,3 +3,9 @@ import Routing from '../../vendor/friendsofsymfony/jsrouting-bundle/Resources/pu
 
 const routes = require('../../public/js/fos_js_routes_website');
 Routing.setRoutingData(routes);
+
+function loadData() {
+    Axios.get(Routing.generate('xhr_room_availability', {}, true)).then((response) => {
+        const data = response.data;
+    })
+}
