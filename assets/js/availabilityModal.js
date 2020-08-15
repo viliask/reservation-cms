8cfd2e4318cf9e44b27e0fe6233503fbf2c11ff7
Kamil Wilas <kamil.wilas@outlook.com>
2020-08-15 16:34:43 +0200

    Add modal handling file
    Import axios and routing
    
    https://trello.com/c/FhumAsQ7

---
+++ assets/js/availabilityModal.js
@@ -0,0 +1,5 @@
+import Axios from 'axios';
+import Routing from '../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
+
+const routes = require('../../public/js/fos_js_routes_website');
+Routing.setRoutingData(routes);
