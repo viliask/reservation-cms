e5e02ca0d8582f8f5009d97cf5ca4ab6f1cc93f2
Kamil Wilas <kamil.wilas@outlook.com>
2020-08-15 16:42:48 +0200

    Pass action parameters
    https://trello.com/c/FhumAsQ7

--- assets/js/availabilityModal.js
+++ assets/js/availabilityModal.js
@@ -5,6 +5,9 @@ const routes = require('../../public/js/fos_js_routes_website');
 Routing.setRoutingData(routes);
 
 const form = document.querySelector('.submit-form');
+const roomId = document.querySelector('h1');
+const checkIn = document.querySelector('#reservation_checkInDate');
+const checkOut = document.querySelector('#reservation_checkOutDate');
 
 function handleForm(event) { event.preventDefault(); }
 form.addEventListener('submit', handleForm);
@@ -14,7 +17,7 @@ form.addEventListener('submit',  () => {
 });
 
 function loadData() {
-    Axios.get(Routing.generate('xhr_room_availability', {}, true)).then((response) => {
+    Axios.get(Routing.generate('xhr_room_availability', {id: roomId.dataset.roomId, checkIn: checkIn.value, checkOut: checkOut.value}, true)).then((response) => {
         const data = response.data;
     })
 }
