ac866f8a2f306181d319e5915af337f494403325
Kamil Wilas <kamil.wilas@outlook.com>
2020-08-27 16:54:44 +0200

    Add close button listener
    https://trello.com/c/tcIFtuRl

--- assets/js/availabilityModal.js
+++ assets/js/availabilityModal.js
@@ -33,7 +33,9 @@ const validateForm = () => {
     const b = document.querySelector('#reservation_checkOutDate').value;
     if (a === null || a === '' || b === null || b === '') {
         document.querySelector('#empty-form-modal').classList.add(visible);
-
+        document.querySelector('[data-close-empty-form]').addEventListener('click', () => {
+            document.querySelector('#empty-form-modal').classList.remove(visible);
+        });
         return false;
     }
     return true;
