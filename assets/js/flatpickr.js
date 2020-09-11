b8abd52ab800c10c2273772a1a1a097942d2430f
Kamil Wilas <kamil.wilas@outlook.com>
2020-09-11 23:38:27 +0200

    Fix datepicker to work with mobiles using another config
    https://trello.com/c/c84LzjGa

--- assets/js/flatpickr.js
+++ assets/js/flatpickr.js
@@ -2,8 +2,15 @@ import flatpickr from 'flatpickr';
 import rangePlugin from 'flatpickr/dist/plugins/rangePlugin';
 import pl from 'flatpickr/dist/l10n/pl';
 
-flatpickr('.js-datepicker', {
-    minDate: 'today',
-    locale: 'pl',
-    plugins: [new rangePlugin({ input: "#reservation_checkOutDate"})]
-});
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    flatpickr('.js-datepicker', {
+        minDate: 'today',
+        locale: 'pl',
+    });
+} else {
+    flatpickr('.js-datepicker', {
+        minDate: 'today',
+        locale: 'pl',
+        plugins: [new rangePlugin({ input: "#reservation_checkOutDate"})],
+    });
+}
