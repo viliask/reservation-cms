0ef848109038c75587d4d044119406552a5ee0dc
Kamil Wilas <kamil.wilas@outlook.com>
2020-10-06 19:51:04 +0200

    Initiate cookieconsent
    https://trello.com/c/6Z5JFVaq

---
+++ assets/js/cookie-info.js
@@ -0,0 +1,17 @@
+window.cookieconsent.initialise({
+    "palette": {
+        "popup": {
+            "background": "#368bdb"
+        },
+        "button": {
+            "background": "#fff",
+            "text": "#368bdb"
+        }
+    },
+    "showLink": false,
+    "theme": "classic",
+    "content": {
+        "message": "Nasza strona internetowa używa plików cookies (tzw. ciasteczka) w celach statystycznych, reklamowych oraz funkcjonalnych. Każdy może zaakceptować  pliki cookies albo ma możliwość wyłączenia ich w przeglądarce, dzięki czemu nie będą zbierane żadne informacje.",
+        "dismiss": "Akceptuję"
+    }
+});
