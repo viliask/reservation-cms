3ac0b828e2e923125b966fdadba490ff05153eca
Kamil Wilas <kamil.wilas@outlook.com>
2020-10-08 18:22:17 +0200

    Add migration for new table
    https://trello.com/c/1yV09eyO

---
+++ migrations/Version20201008161726.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20201008161726 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $this->addSql('CREATE TABLE reservation_settings (id INT AUTO_INCREMENT NOT NULL, winter_start DATETIME NOT NULL, winter_end DATETIME NOT NULL, price_modifier SMALLINT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->addSql('DROP TABLE reservation_settings');
+    }
+}
