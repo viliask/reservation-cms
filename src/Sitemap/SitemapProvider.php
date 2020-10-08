5df091d446ef675e69deb9a2be7b08353b00eaa0
Kamil Wilas <kamil.wilas@outlook.com>
2020-10-08 14:05:07 +0200

    Add sitemap for rooms entries
    https://trello.com/c/FSBjEI34

---
+++ src/Sitemap/SitemapProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Sitemap;
+
+use App\Repository\RoomRepository;
+use Sulu\Bundle\WebsiteBundle\Sitemap\Sitemap;
+use Sulu\Bundle\WebsiteBundle\Sitemap\SitemapProviderInterface;
+use Sulu\Bundle\WebsiteBundle\Sitemap\SitemapUrl;
+
+class SitemapProvider implements SitemapProviderInterface
+{
+    /**
+     * @var RoomRepository
+     */
+    private $repository;
+
+    /**
+     * @param RoomRepository $repository
+     */
+    public function __construct(RoomRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function build($page, $scheme, $host)
+    {
+        $result = [];
+        foreach ($this->repository->findEnabled(50000) as $item) {
+            $result[] = new SitemapUrl(
+                $scheme . '://' . $host . '/' . $item->getSlug(),
+                'pl',
+                'pl',
+                null,
+                null,
+                80
+            );
+        }
+        $this->repository->countEnabled();
+
+        return $result;
+    }
+
+    public function getAlias()
+    {
+        return 'rooms';
+    }
+
+    public function createSitemap($scheme, $host)
+    {
+        return new Sitemap($this->getAlias(), $this->getMaxPage($scheme, $host));
+    }
+
+    public function getMaxPage($scheme, $host)
+    {
+        return ceil($this->repository->countEnabled() / self::PAGE_SIZE);
+    }
+}
